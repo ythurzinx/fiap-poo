9fcc9546ed158c1d142736baefcc4643a8895733
Arthur <alencararthur47@gmail.com>
2026-03-10 20:11:26 -0300

    feat: adiciona métodos de comportamento

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -5,25 +5,25 @@ import br.com.fiapride.model.Teclado;
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
+
         Teclado teclado1 = new Teclado();
         teclado1.luzLayout = "ABNT2";
-        teclado1.mudarCorLed = "Verde, Vermelho ou Azul";
+        teclado1.mudarCorLed = "Verde";
         teclado1.larguraEmCm = 44;
         teclado1.quantidadeTeclas = 61;
         teclado1.alturaEmCm = 5;
 
+        System.out.println("--- Estado inicial do teclado ---");
+        teclado1.exibirInformacoes();
 
+        System.out.println("\n--- Teste com valor válido ---");
+        teclado1.alterarCorLed("Azul");
+        teclado1.alterarQuantidadeTeclas(87);
+        teclado1.exibirInformacoes();
 
-        Teclado teclado2 = new Teclado();
-        teclado2.luzLayout = "US";
-        teclado2.mudarCorLed = "Ativar RBG?";
-        teclado2.larguraEmCm = 50;
-        teclado2.quantidadeTeclas = 84;
-        teclado2.alturaEmCm = 8;
-
-
-        System.out.println("--- Sistema FiapRide Teclados Novos ---");
-        System.out.println("Layout: " + teclado1.luzLayout + " | Cor do Led: " + teclado1.mudarCorLed + " | Largura em CM: " + teclado1.larguraEmCm + " | Quantidade de Teclas: " + teclado1.quantidadeTeclas + " | Altura em CM: " + teclado1.alturaEmCm);
-        System.out.println("Layout: " + teclado2.luzLayout + " | Cor do Led: " + teclado2.mudarCorLed + " | Largura: em CM" + teclado2.larguraEmCm + " | Quantidade de Teclas: " + teclado2.quantidadeTeclas + " | Altura em CM: " + teclado2.alturaEmCm);
+        System.out.println("\n--- Teste com valor inválido ---");
+        teclado1.alterarCorLed("");
+        teclado1.alterarQuantidadeTeclas(-10);
+        teclado1.exibirInformacoes();
     }
 }
--- src/br/com/fiapride/model/Teclado.java
+++ src/br/com/fiapride/model/Teclado.java
@@ -1,11 +1,38 @@
 package br.com.fiapride.model;
 
+public class Teclado {
 
-public class Teclado    {
-
-    public String mudarCorLed;
     public String luzLayout;
-    public float alturaEmCm;
+    public String mudarCorLed;
     public float larguraEmCm;
-    public double quantidadeTeclas;
+    public float alturaEmCm;
+    public int quantidadeTeclas;
+
+    public void exibirInformacoes() {
+        System.out.println(
+                "Layout: " + luzLayout +
+                        " | Cor do Led: " + mudarCorLed +
+                        " | Largura em CM: " + larguraEmCm +
+                        " | Quantidade de Teclas: " + quantidadeTeclas +
+                        " | Altura em CM: " + alturaEmCm
+        );
+    }
+
+    public void alterarCorLed(String novaCor) {
+        if (novaCor == null || novaCor.isEmpty()) {
+            System.out.println("Erro: a cor do LED não pode ser vazia.");
+        } else {
+            mudarCorLed = novaCor;
+            System.out.println("Cor do LED alterada para: " + mudarCorLed);
+        }
+    }
+
+    public void alterarQuantidadeTeclas(int novaQuantidade) {
+        if (novaQuantidade <= 0) {
+            System.out.println("Erro: quantidade de teclas inválida.");
+        } else {
+            quantidadeTeclas = novaQuantidade;
+            System.out.println("Quantidade de teclas alterada para: " + quantidadeTeclas);
+        }
+    }
 }
